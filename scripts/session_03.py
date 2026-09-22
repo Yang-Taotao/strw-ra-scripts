f7055bb4a9a5dfc51ad2f420e8a23f7bee378dfa
T. Yang <yangtaotao0524@gmail.com>
2026-09-22 21:25:19 +0200

    feat: add session_03 spectrum plot utilities - add loaders for spectrum and dynamic spectrum bin data - add plotters for spectrum and dynamic spectrum - add logging prints - add spectrum and dynamic spectrum plots - remove unused `common_plot()` - clean up code and run linter pass

--- scripts/session_03.py
+++ scripts/session_03.py
@@ -1,100 +1,143 @@
-# %%
-# you can run this in interactive python mode
+"""
+Radio Astronomy Session 03 Scripts
+- Use `./env.yml` for `conda` managed env replication
+- Run with `python ./scripts/session_03.py`, see detail in `./README.md`
+- See below for local directory assignment
+"""
 
-# lib
-import numpy as np
+# imports
+from pathlib import Path
+
 import matplotlib.pyplot as plt
-import itertools
+import numpy as np
+import scienceplots  # noqa: F401
 
-import scienceplots
+# plt cfg
+plt.style.use(["science"])
 
-# cfg
-plt.style.use(["science", "no-latex", "notebook"])
+# path assignment
+DIR_SCRIPTS = Path(__file__).resolve().parent
+DIR_BASE = DIR_SCRIPTS.parent
+DIR_DATA = DIR_BASE / "data"
+DIR_FIG = DIR_BASE / "fig"
 
-# %%
-# local var
-filename_raw = "../data/session_03_data_raw"
-filename_fft = "../data/session_03_data_fft"
+PATH_RAW = DIR_DATA / "session_03_data_raw"
+PATH_FFT = DIR_DATA / "session_03_data_fft"
+PATH_INT = DIR_DATA / "session_03_data_fft_int"
 
-fig_path = "../fig/session_03.pdf"
+PATH_FIG = DIR_FIG / "session_03.pdf"
+PATH_FIG_SPECTRUM = DIR_FIG / "session_03_spectrum.pdf"
+PATH_FIG_WATERFALL = DIR_FIG / "session_03_waterfall.pdf"
 
-f_raw = np.fromfile(open(filename_raw), dtype=np.complex64)
-f_fft = np.fromfile(open(filename_fft), dtype=np.complex64)
+# local const
+SAMP_RATE = 1e7  # 10 MHz
+CENTRAL_FREQ = 1.42e9  # 1.42 GHz
+TGT_SAMP_FREQ = 2.84e9  # 2.84 GHz
+SAMP_SPACING = 1 / SAMP_RATE
+FFT_SIZE = 2048  # N
+ERR = 1e-32
 
-sample_rate = 1e6
-tgt_sample_freq = 2.84e9
-size = len(f_raw)
-
-# cast to real and imag
-f_raw_r, f_raw_i = f_raw.real, f_raw.imag
-f_fft_r, f_fft_i = f_fft.real, f_fft.imag
-
-# %%
-# time and freq
-# time
-t = np.linspace(0, size, size, dtype=np.float64)
-t *= 1 / sample_rate
-
-f_fft_manual = f_fft_r**2 + f_fft_i**2
-
-# freq
-freq_fft = np.linspace(0, tgt_sample_freq, len(f_fft))
-
-freq_fft_r = np.fft.fftfreq(len(f_fft_r), 1e-7)
-freq_fft_i = np.fft.fftfreq(len(f_fft_i), 1e-7)
-freq_fft_manual = np.fft.fftfreq(len(f_fft_manual), 1e-7)
-
-# %%
-# plt
-
-n = int(0.25 * len(t))
+# data loader
 
 
-def common_plot() -> None:
-    """simple plotter"""
-    fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(7, 1, figsize=(12, 21))
+def load_spectrum(path):
+    """read bin data from path and return freq, amp"""
 
-    color_cycle = itertools.cycle(plt.rcParams["axes.prop_cycle"].by_key()["color"])
+    # read data_fft_int from bin
+    data = np.fromfile(path, dtype=np.float32)
+    # reshape
+    # data.shape should now be (fft_size, xxx)
+    data = data.reshape(-1, FFT_SIZE)
+    # average into (fft_size, )
+    amp = np.nanmean(data, axis=0)
 
-    ax1.plot(t[0:n], f_raw_r[0:n], color=next(color_cycle))
-    ax1.set_xlabel("t (s)")
-    ax1.set_ylabel("amp_real (raw)")
-    ax1.set_title("data_real vs t")
+    # center around central freq at Hz
+    base = np.fft.fftfreq(FFT_SIZE, d=SAMP_SPACING)
+    freq = CENTRAL_FREQ + np.fft.fftshift(base)
 
-    ax2.plot(t[0:n], f_raw_i[0:n], color=next(color_cycle))
-    ax2.set_xlabel("t (s)")
-    ax2.set_ylabel("amp_imag (raw)")
-    ax2.set_title("data_imag vs t")
+    return freq, amp
 
-    ax3.plot(freq_fft, f_fft_r, color=next(color_cycle))
-    ax3.set_xlabel("freq_fft")
-    ax3.set_ylabel("amp_real (fft)")
-    ax3.set_title("data_fft_real vs freq")
 
-    ax4.plot(freq_fft, f_fft_i, color=next(color_cycle))
-    ax4.set_xlabel("freq_fft")
-    ax4.set_ylabel("amp_imag (fft)")
-    ax4.set_title("data_fft_imag vs freq")
+def load_waterfall(path):
+    """read bin data from path and return freq, time, and gain"""
 
-    ax5.plot(freq_fft_r, f_fft_r, color=next(color_cycle))
-    ax5.set_xlabel("freq_fft_real")
-    ax5.set_ylabel("amp_imag (fft)")
-    ax5.set_title("data_fft_real vs freq_fft_real")
+    # read data_fft from bin
+    data = np.fromfile(path, dtype=np.complex64)
+    # reshape
+    data = data.reshape(-1, FFT_SIZE)
 
-    ax6.plot(freq_fft_i, f_fft_i, color=next(color_cycle))
-    ax6.set_xlabel("freq_fft_imag")
-    ax6.set_ylabel("amp_imag (fft)")
-    ax6.set_title("data_fft_imag vs freq_fft_imag")
+    # get complex to mag**2
+    gain = np.abs(data) ** 2
+    # cast to db at log10 with small ERR for numerical stability
+    gain = 10 * np.log10(gain + ERR)
 
-    ax7.plot(freq_fft_manual, f_fft_manual, color=next(color_cycle))
-    ax7.set_xlabel("freq_fft_manual")
-    ax7.set_ylabel("amp_manual (fft)")
-    ax7.set_title("data_fft_manual vs freq_fft_manual")
+    # convert to s
+    time = np.arange(len(data)) * FFT_SIZE * SAMP_SPACING
 
-    plt.tight_layout()
-    plt.show()
-    plt.savefig(fig_path, dpi=300, format="pdf")
+    # center around central freq at Hz
+    base = np.fft.fftfreq(FFT_SIZE, d=SAMP_SPACING)
+    freq = CENTRAL_FREQ + np.fft.fftshift(base)
+
+    return freq, time, gain
+
+
+# plt - main
+
+
+def plot_spectrum(freq, amp, path) -> None:
+    """spectrum plot"""
+    # fig
+    fig, ax = plt.subplots()
+    ax.plot(freq, amp)
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Amplitude")
+
+    fig.tight_layout()
+    fig.savefig(path, dpi=300, format="pdf")
     plt.close(fig)
 
 
-common_plot()
+def plot_waterfall(freq, time, gain, path) -> None:
+    """waterfall plot"""
+    # local repo
+    extent = (float(time[0]), float(time[-1]), float(freq[0]), float(freq[-1]))
+    vmin, vmax = np.percentile(gain, [5, 99.99])
+
+    # fig
+    fig, ax = plt.subplots()
+    im = ax.imshow(
+        gain.T,
+        origin="lower",
+        aspect="auto",
+        extent=extent,
+        cmap="inferno",
+        vmin=vmin,
+        vmax=vmax,
+    )
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Freqency (Hz)")
+    fig.colorbar(im, ax=ax, label="Relative Gain (dB)")
+
+    fig.tight_layout()
+    fig.savefig(path, dpi=300, format="pdf")
+    plt.close(fig)
+
+
+def main():
+    """wrapper for main call"""
+
+    print("SESSION03")
+    print("=" * 88)
+    print(f"         PROCESSING {PATH_INT}")
+    arg_tmp = load_spectrum(PATH_INT)
+    plot_spectrum(*arg_tmp, PATH_FIG_SPECTRUM)
+    print(f"         PLOT SAVED {PATH_FIG_SPECTRUM}")
+    print(f"         PROCESSING {PATH_FFT}")
+    arg_tmp = load_waterfall(PATH_FFT)
+    plot_waterfall(*arg_tmp, PATH_FIG_WATERFALL)
+    print(f"         PLOT SAVED {PATH_FIG_WATERFALL}")
+    print("=" * 88)
+
+
+if __name__ == "__main__":
+    main()
